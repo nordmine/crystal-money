d8d882cd45304200beefd3b8e85251902a958077
Boris Nikolaev <nordmine@gmail.com>
2013-12-18 00:08:44 +0400

    comment in mts parser

--- CrystalMoneyProject/CrystalMoney/src/main/java/ru/nordmine/crystalmoney/sms/MtsBankParser.java
+++ CrystalMoneyProject/CrystalMoney/src/main/java/ru/nordmine/crystalmoney/sms/MtsBankParser.java
@@ -11,8 +11,8 @@ public class MtsBankParser implements SmsParser {
     public List<PatternData> getMessagePatterns() {
         List<PatternData> patterns = new ArrayList<PatternData>();
         patterns.add(new PatternData(INCOME, false, "Приход по счету карты \\w+ \\*(\\d{4}); ([\\s\\d,]+) RUB;"));
-        patterns.add(new PatternData(OUTCOME, true, "Наличные \\w+ \\*(\\d{4}); \\d{2}\\.\\d{2} \\d{2}:\\d{2}; .*?; ([\\d\\s\\,]+) RUB;"));
-        patterns.add(new PatternData(OUTCOME, false, "Оплата \\w+ \\*(\\d{4}); \\d{2}\\.\\d{2} \\d{2}:\\d{2}; .*?; ([\\d\\s\\,]+) RUB;"));
+        patterns.add(new PatternData(OUTCOME, true, "Наличные \\w+ \\*(\\d{4}); \\d{2}\\.\\d{2} \\d{2}:\\d{2}; (.*?); ([\\d\\s\\,]+) RUB;"));
+        patterns.add(new PatternData(OUTCOME, false, "Оплата \\w+ \\*(\\d{4}); \\d{2}\\.\\d{2} \\d{2}:\\d{2}; (.*?); ([\\d\\s\\,]+) RUB;"));
         return patterns;
     }
 
@@ -22,6 +22,13 @@ public class MtsBankParser implements SmsParser {
             ParsingResult result = new ParsingResult();
             result.setCardNumber(matcher.group(1));
             result.setAmount(new BigDecimal(matcher.group(2).replaceAll(" ", "").replace(",", ".")));
+            return result;
+        }
+        if (matcher.groupCount() == 3) {
+            ParsingResult result = new ParsingResult();
+            result.setCardNumber(matcher.group(1));
+            result.setComment(matcher.group(2));
+            result.setAmount(new BigDecimal(matcher.group(3).replaceAll(" ", "").replace(",", ".")));
             return result;
         }
         return null;
