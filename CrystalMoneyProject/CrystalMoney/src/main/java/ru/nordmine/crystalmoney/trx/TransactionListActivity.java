d6fbf75675e1c4a8f01dcab513a613e8eb5ee5a8
Boris Nikolaev <nordmine@gmail.com>
2014-03-08 00:10:53 +0400

    monthly paging in exchange list

--- CrystalMoneyProject/CrystalMoney/src/main/java/ru/nordmine/crystalmoney/trx/TransactionListActivity.java
+++ CrystalMoneyProject/CrystalMoney/src/main/java/ru/nordmine/crystalmoney/trx/TransactionListActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -23,14 +21,13 @@ import java.text.DateFormatSymbols;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import ru.nordmine.crystalmoney.MainActivity;
 import ru.nordmine.crystalmoney.R;
 import ru.nordmine.crystalmoney.account.AccountDao;
 
-public abstract class TransactionListActivity extends FragmentActivity implements ActionBar.TabListener {
+public abstract class TransactionListActivity extends Activity implements ActionBar.TabListener {
 
     protected ListView listView;
     protected TextView pagerTextView;
@@ -41,11 +38,14 @@ public abstract class TransactionListActivity extends FragmentActivity implement
     protected AccountDao accountDao = new AccountDao(this);
     protected Calendar startCalendar = Calendar.getInstance();
     protected Calendar endCalendar = Calendar.getInstance();
-    protected int calendarField = Calendar.DAY_OF_MONTH;
+    protected int calendarField;
+    protected int dayOfMonthInDailyMode;
     private Resources res;
 
     private static final String MY_PREFS = "my_prefs1";
-    private static final String SELECTED_TAB_INDEX = "selected_tab_name";
+    private static final String START_DATE = "start_date";
+    private static final String CALENDAR_FIELD = "calendar_field";
+    private static final String DAY_OF_MONTH_IN_DAILY_MODE = "day_of_month_daily_mode";
     private SharedPreferences preferences;
 
     @Override
@@ -78,6 +78,9 @@ public abstract class TransactionListActivity extends FragmentActivity implement
 
         res = getResources();
         preferences = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE);
+        startCalendar.setTimeInMillis(preferences.getLong(START_DATE, startCalendar.getTimeInMillis()));
+        calendarField = preferences.getInt(CALENDAR_FIELD, Calendar.DAY_OF_MONTH);
+        dayOfMonthInDailyMode = preferences.getInt(DAY_OF_MONTH_IN_DAILY_MODE, 1);
 
         ActionBar.Tab dayTab = actionBar.newTab().setText(res.getString(R.string.caption_day)).setTabListener(this);
         actionBar.addTab(dayTab);
@@ -85,23 +88,30 @@ public abstract class TransactionListActivity extends FragmentActivity implement
         actionBar.addTab(monthTab);
 
         selectTabByPreferences();
+        initEndCalendar();
     }
 
     private void selectTabByPreferences() {
         ActionBar actionBar = getActionBar();
-        int selectedTabIndex = preferences.getInt(SELECTED_TAB_INDEX, 0);
+        int selectedTabIndex = -1;
+        if (calendarField == Calendar.DAY_OF_MONTH) {
+            selectedTabIndex = 0;
+            startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonthInDailyMode);
+        }
+        if (calendarField == Calendar.MONTH) {
+            selectedTabIndex = 1;
+        }
         actionBar.setSelectedNavigationItem(selectedTabIndex);
     }
 
     private void initEndCalendar() {
         endCalendar.setTimeInMillis(startCalendar.getTimeInMillis());
-        endCalendar.set(calendarField, endCalendar.get(calendarField) + 1);
+        endCalendar.add(calendarField, 1);
     }
 
     protected void refreshItems() {
         trxItems = dao.getAll(startCalendar.getTimeInMillis(), endCalendar.getTimeInMillis());
 
-        Date d = new Date(startCalendar.getTimeInMillis());
         SimpleDateFormat sdf = new SimpleDateFormat("d MMMM yyyy");
         if (calendarField == Calendar.MONTH) {
             String[] monthNames = res.getStringArray(R.array.month_names);
@@ -109,7 +119,7 @@ public abstract class TransactionListActivity extends FragmentActivity implement
             russianSymbols.setMonths(monthNames);
             sdf = new SimpleDateFormat("MMMM yyyy", russianSymbols);
         }
-        pagerTextView.setText(sdf.format(d));
+        pagerTextView.setText(sdf.format(startCalendar.getTime()));
 
         listView.setAdapter(new TransactionItemAdapter(this, android.R.layout.simple_list_item_1,
                 trxItems.toArray(new TransactionItem[trxItems.size()])));
@@ -178,13 +188,13 @@ public abstract class TransactionListActivity extends FragmentActivity implement
     protected abstract void onAddButtonClick();
 
     public void onPrevButtonClick(View v) {
-        startCalendar.set(calendarField, startCalendar.get(calendarField) - 1);
+        startCalendar.add(calendarField, -1);
         initEndCalendar();
         refreshItems();
     }
 
     public void onNextButtonClick(View v) {
-        startCalendar.set(calendarField, startCalendar.get(calendarField) + 1);
+        startCalendar.add(calendarField, 1);
         initEndCalendar();
         refreshItems();
     }
@@ -193,6 +203,7 @@ public abstract class TransactionListActivity extends FragmentActivity implement
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         if (tab.getText().equals(res.getString(R.string.caption_day))) {
             calendarField = Calendar.DAY_OF_MONTH;
+            startCalendar.set(calendarField, dayOfMonthInDailyMode);
         }
         if (tab.getText().equals(res.getString(R.string.caption_month))) {
             startCalendar.set(Calendar.DAY_OF_MONTH, 1);
@@ -204,6 +215,9 @@ public abstract class TransactionListActivity extends FragmentActivity implement
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        if (tab.getText().equals(res.getString(R.string.caption_day))) {
+            dayOfMonthInDailyMode = startCalendar.get(Calendar.DAY_OF_MONTH);
+        }
     }
 
     @Override
@@ -216,7 +230,14 @@ public abstract class TransactionListActivity extends FragmentActivity implement
     protected void onPause() {
         super.onPause();
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt(SELECTED_TAB_INDEX, getActionBar().getSelectedNavigationIndex());
+        int selectedIndex = getActionBar().getSelectedNavigationIndex();
+        editor.putInt(CALENDAR_FIELD, calendarField);
+        if (selectedIndex == 0) {
+            editor.putInt(DAY_OF_MONTH_IN_DAILY_MODE, startCalendar.get(Calendar.DAY_OF_MONTH));
+        } else {
+            editor.putInt(DAY_OF_MONTH_IN_DAILY_MODE, dayOfMonthInDailyMode);
+        }
+        editor.putLong(START_DATE, startCalendar.getTimeInMillis());
         editor.apply();
     }
 
